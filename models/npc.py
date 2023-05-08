91d3f63482efee092e19efd7b522a08608ff4c73
JonathanJoelVos <dev.jonathan.joel.vos@gmail.com>
2023-05-08 19:17:21 -0300

    npc implementado

---
+++ models/npc.py
@@ -0,0 +1,6 @@
+from personagem import Personagem
+from classe import Classe
+
+class Npc(Personagem):
+    def __init__(self, nome: str, classe: Classe, nivel: int):
+        super().__init__(nome, classe, nivel)
